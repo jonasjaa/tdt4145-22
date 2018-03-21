8b8117a22937d76938237bf5db74e91a3f4b1c65
unknown <jonasjaas@gmail.com>
2018-03-21 13:14:32 +0100

    La til icon

--- src/main/App.java
+++ src/main/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,6 +14,7 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Treningsdagbok");
+        stage.getIcons().add(new Image("http://i68.tinypic.com/v5kac1.png"));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
